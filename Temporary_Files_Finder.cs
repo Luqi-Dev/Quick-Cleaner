7adc3ab59da300283ce1c3e15a88edb04cb46368
Luqi-Dev <127991283+Luqi-Dev@users.noreply.github.com>
2023-04-11 18:02:57 +0300

    Add files via upload

---
+++ Temporary_Files_Finder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace Quick_Clean
+{
+	public class Temporary_Files_Finder : Window
+	{
+		public int Final;
+		public long Size;
+		public string TempPath;
+		public DirectoryInfo TempSize;
+		
+		public void Files_Finder()
+		{
+			TempPath = Path.GetTempPath();
+			TempSize = new DirectoryInfo(TempPath);
+		}
+		
+		public void Size_Viewer()
+		{
+			foreach(FileInfo FI in TempSize.EnumerateFiles("*", SearchOption.AllDirectories))
+			{
+			Size += FI.Length;
+			}
+			
+			double ExactSize = (double)Size / (1024*1024);
+			
+			Final = (int) ExactSize;
+		}
+	}
+}
---
+++ WhiteList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Quick_Clean
+{
+	public class WhiteList
+	{
+		public void Kill()
+		{
+			var processes = Process.GetProcesses();
+			
+			foreach(var process in processes)
+			{
+				var FileName = process.MainModule.FileName;
+				if(!FileName.StartsWith(Environment.SystemDirectory))
+				{
+					process.Kill();
+				}
+			}
+			
+		}
+	}
+}
