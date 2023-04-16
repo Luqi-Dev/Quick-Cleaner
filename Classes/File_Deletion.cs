698229acdaca8051b07ca99367cfb29a2505dd3e
Luqi-Dev <127991283+Luqi-Dev@users.noreply.github.com>
2023-04-16 10:36:44 +0300

    Add files via upload

---
+++ Classes/File_Deletion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Quick_Clean
+{
+	public class FileDeletion
+	{
+		public void FileDeleter()
+		{
+			
+			List<string> lockedFiles = new List<string>();
+			PathFinder pathFinder = new PathFinder();
+			pathFinder.Files_Finder();
+			
+            foreach (string Files in Directory.GetFiles(pathFinder.TempPath))
+            {   
+           		try
+    			{
+        			using (FileStream stream = File.Open(Files, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+        			{
+        				File.Delete(Files);
+        			}
+    			}
+           		
+    			catch (IOException)
+    			{
+        			lockedFiles.Add(Files);
+    			}
+
+            }
+
+            string FileMessage = "The following files were unable to detele:\n";
+            foreach(string Unable in lockedFiles)
+            {
+            	FileMessage += Unable + "\n";
+            }   
+
+			System.Windows.Forms.MessageBox.Show(FileMessage);
+			
+		}
+	}
+}
